22073553947789ad74fd7510748f5924f564b75f
Ashley <ashleyoy27@gmail.com>
2025-08-10 17:37:50 +0800

    Main,Death,Win Screen Assets Implemented

--- Assets/Scripts/UI/WinMenu.cs
+++ Assets/Scripts/UI/WinMenu.cs
@@ -5,16 +5,28 @@ using UnityEngine.UI;
 
 public class WinMenu : MonoBehaviour
 {
-	[SerializeField] private Button mainMenu;
+    [SerializeField] private Image title;
+    [SerializeField] private Button restart;
+    [SerializeField] private Button mainMenu;
 
 	private void Start()
 	{
-		// Set up button animations
-		mainMenu.transform.localScale = Vector3.zero;
+        title.rectTransform.DOScale(1.2f, 0.5f).SetEase(Ease.OutBounce).SetDelay(0.2f).OnComplete(() =>
+        {
+            title.rectTransform.DOScale(1f, 0.5f).SetEase(Ease.InOutQuad);
+        });
+        // Set up button animations
+        mainMenu.transform.localScale = Vector3.zero;
 		mainMenu.transform.DOScale(3f, 0.5f).SetEase(Ease.OutBack);
-	}
+        restart.transform.localScale = Vector3.zero;
+        restart.transform.DOScale(1f, 0.5f).SetEase(Ease.OutBack).SetDelay(0.5f);
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
 
-	public void MainMenu()
+    public void MainMenu()
 	{
 		SceneManager.LoadScene("MainMenu");
 	}
