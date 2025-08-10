e1d7e510f1c79bfe76c0eff927e5254104da9918
AsValen <75420108+AsValen@users.noreply.github.com>
2025-08-10 14:03:14 +0800

    attached script to prefab

--- Assets/Scripts/Obstacles/Spike.cs
+++ Assets/Scripts/Obstacles/Spike.cs
@@ -6,11 +6,13 @@ public class Spike : MonoBehaviour
     //Add changing of opacity of player
     void OnCollisionEnter2D(Collision2D collision)
     {
+        //Debug.Log("spikeOn");
         if(collision.gameObject.CompareTag("Player"))
         {
             PlayerStats playerStats = collision.gameObject.GetComponent<PlayerStats>();
             if(playerStats != null)
             {
+                //Debug.Log("spike");
                 playerStats.SetPlayerHP(1); // Assuming spikes deal 1 damage
             }
         }
